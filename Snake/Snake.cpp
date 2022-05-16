24fb6102e6142fdb4f5de6d19950503a8cffb6fd
NovikovNick <nicknovikov10@gmail.com>
2022-05-16 22:48:18 +0300

    initial

---
+++ Snake/Snake.cpp
@@ -0,0 +1,15 @@
+﻿// Snake.cpp : Defines the entry point for the application.
+//
+
+#include "Snake.h"
+#include <windows.h>
+// #include <d2d1.h>
+
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
